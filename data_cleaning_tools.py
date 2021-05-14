450a6cc3e8882b83d108a7e6901b57843aaf6b0d
Andreas404 <andreas.fettweis@kuleuven.be>
2021-05-14 16:54:05 +0200

    Update preprocessing

--- data_cleaning_tools.py
+++ data_cleaning_tools.py
@@ -1,8 +1,7 @@
-"""
-This script includes useful tools that do not need to be explicitly shown in the main notebook/app.
-Some of the tools have been turnde into classes as they perform similar tasks.
-This allows for better organisation and ease of use.
-"""
+# This script includes useful tools that do not need to be explicitly shown in the main notebook/app.
+# Some of the tools have been turned into classes as they perform similar tasks.
+# This allows for better organisation and ease of use.
+
 
 import pandas as pd
 import numpy as np
@@ -10,10 +9,10 @@ import country_converter as coco
 
 
 class data_cleaner():
-    """ Class that includes different methods which import and perform data cleaning on specific datasets. """
+    # Class that includes different methods which import and perform data cleaning on specific datasets.
 
     def temp_proj(self, file_name):
-        """Imports and cleans the forecasted temperatures"""
+        # Imports and cleans the forecasted temperatures
 
         df = pd.read_csv('raw data/tas_2020_2039_mavg_rcp26_AFG_CAF.csv', sep = ',', header = 0, skipinitialspace = True, usecols = range(0,6))
         df.rename({df.columns[0]: "Temperature (°C)", df.columns[1]: "Year", df.columns[2]: "Model", df.columns[3]: "Month", df.columns[4]: "Name", df.columns[5]: "Country"}, axis = 1, inplace = True)
@@ -24,14 +23,16 @@ class data_cleaner():
         df['Weight'] = df['Month'].map(weights)
         df = df.groupby('Country').apply(lambda x: (x['Temperature (°C)'] * x['Weight']).sum() / x['Weight'].sum())
         df = pd.DataFrame(df).rename(columns={0:'Temperature (°C)'})
+        
         df.reset_index(inplace=True)
         
         return df
 
     def socioecon_factors(self, aquastat_file_name, unicef_file_name):
-        """Imports and cleans the socioeconimic factor datasets - aquastat and unicef """
+        # Imports and cleans the socioeconimic factor datasets - aquastat and unicef
+        
+        # Aquastat
         df_aqua = pd.read_csv(aquastat_file_name)
-
         df_aqua = df_aqua.rename(columns = {'Unnamed: 0':'Country', 'Unnamed: 1':'Variable'})
         aqua_named_cols = [c for c in df_aqua.columns if c.lower()[:7] != 'unnamed']
         df_aqua = df_aqua[aqua_named_cols]
@@ -40,31 +41,28 @@ class data_cleaner():
         aqua_missing_per_var = df_aqua.groupby(['Variable']).apply(lambda x: x.isnull().sum()).sum(axis = 1).sort_values()
         df_aqua = df_aqua[~df_aqua.isin(["Total water withdrawal per capita (m<sup>3</sup>/year per inhabitant)", 'Gender Inequality Index (GII) [equality = 0; inequality = 1) (-)']).any(axis=1)]
         df_aqua = df_aqua.dropna(subset = ['Variable'], axis = 0)
-
+         
+        # Unicef
         df_unicef = pd.read_csv(unicef_file_name)
         unicef_keep_cols = ['Indicator', 'Country', 'Time', 'Value']
         df_unicef = df_unicef[unicef_keep_cols]
         unicef_missing_per_var = df_unicef.groupby(['Indicator']).apply(lambda x: x.isnull().sum()).sum(axis = 1).sort_values()
         remove_vars = unicef_missing_per_var[ unicef_missing_per_var > 60].index
         df_unicef = df_unicef[ ~df_unicef.isin(remove_vars).any(axis = 1)]
-
         aqua_missing_per_country = df_aqua.groupby(['Country']).apply(lambda x: x.isnull().sum()).sum(axis = 1).sort_values()
         aqua_countries = aqua_missing_per_country[aqua_missing_per_country == 0].index
         df_aqua = df_aqua[df_aqua['Country'].isin(aqua_countries)]
         unicef_missing_per_country = df_unicef.groupby(['Country']).apply(lambda x: x.isnull().sum()).sum(axis = 1).sort_values()
         unicef_countries =  unicef_missing_per_country[unicef_missing_per_country == 0].index
         df_unicef = df_unicef[df_unicef['Country'].isin(unicef_countries)]
-
+  
         # Replacing country names by ISO3 country codes.
         df_aqua.replace(to_replace = 'Grenade', value = 'Grenada', inplace = True)
         df_unicef.replace(to_replace = 'Grenade', value = 'Grenada', inplace = True)
-
         df_aqua = df_aqua.drop(df_aqua[df_aqua.Country == 'Channel Islands'].index)
         df_unicef = df_unicef.drop(df_unicef[df_unicef.Country == 'Channel Islands'].index)
-
         df_aqua['Country'] = coco.convert(names = df_aqua['Country'], to = 'ISO3', not_found = 'XXX')
         df_aqua = df_aqua[~df_aqua.isin(['XXX']).any(axis = 1)]
-
         df_unicef['Country'] = coco.convert(names = df_unicef['Country'], to = 'ISO3', not_found = 'XXX')
         df_unicef = df_unicef[~df_unicef.isin(['XXX']).any(axis = 1)]
 
@@ -72,7 +70,6 @@ class data_cleaner():
         C_aqua = df_aqua['Country'].unique()
         C_unicef = df_unicef['Country'].unique()
         C_shared = set.intersection(set(C_aqua), set(C_unicef))
-
         df_aqua = df_aqua[df_aqua['Country'].isin(C_shared)]
         df_unicef = df_unicef[df_unicef['Country'].isin(C_shared)]
         
@@ -91,15 +88,19 @@ class data_cleaner():
         df_unicef.set_index('Country',inplace=True)
         df_socioec_factors = pd.merge(df_aqua,df_unicef,left_index=True,right_index=True)
         
-        df_aqua.reset_index(inplace=True)
-        df_unicef.reset_index(inplace=True)
-        df_socioec_factors.reset_index(inplace=True)
+        # Rename variables
+        df_socioec_factors.rename(columns = {'Human Development Index (HDI) [highest = 1] (-)': 'HDI', 'Rural population (1000 inhab)' : 'rural_pop', 'Urban population (1000 inhab)' : 'urban_pop', 'Rural population with access to safe drinking-water (JMP) (%)' : 'rural_water', 'Urban population with access to safe drinking-water (JMP) (%)' : 'urban_water', 'Life expectancy at birth, total (years)':'life_ex', 'Mortality rate, infant (per 1,000 live births)': 'mort_rate', 'Population growth (annual %)': 'pop_growth', 'GDP per capita, PPP (constant 2011 international $)': 'GDP_pcp'}, inplace=True)
         
-        return df_aqua, df_unicef, df_socioec_factors
+        # Add two additional variables
+        df_socioec_factors['r_u'] = pd.to_numeric(df_socioec_factors['rural_pop']) / pd.to_numeric(df_socioec_factors['urban_pop'])
+        df_socioec_factors['r_u_access'] = pd.to_numeric(df_socioec_factors['rural_water']) / pd.to_numeric(df_socioec_factors['urban_water'])
+        
+        df_socioec_factors.reset_index(inplace=True)
+                
+        return df_socioec_factors
 
     def climate_factors(self, rainfall_file_name, temperature_file_name, water_resources_file_name):
-        
-        """Imports and cleans the climate factor dataset"""
+        # Imports and cleans the climate factor dataset
         
         # Rainfall - 2013-2017
         df_rain = pd.read_csv(rainfall_file_name, skipinitialspace = True).rename(columns = {'Rainfall - (MM)':'Total Rainfall (mm)'})
@@ -136,19 +137,32 @@ class data_cleaner():
         # Merge Factors
         df_climate_factors = pd.merge(df_temp,df_rain,left_index=True,right_index=True)
         df_climate_factors = pd.merge(df_climate_factors,df_water,left_index=True,right_index=True,how='outer')
+        
+        # Rename variables
+        df_climate_factors.rename(columns={'Temperature (°C)':'Temp','Total Rainfall (mm)':'Rain','Total internal renewable water resources (IRWR)':'IRWR','Total external renewable water resources (ERWR)':'ERWR','Total renewable water resources':'TRWR','Dependency ratio':'Dep_ratio'}, inplace=True)
+        
+        # Recalculate water resource variables
+        df_climate_factors['IRWR'].fillna(0,inplace=True)
+        df_climate_factors['ERWR'].fillna(0,inplace=True)
+        df_climate_factors['TRWR'] = df_climate_factors['IRWR']+df_climate_factors['ERWR']
+        df_climate_factors['Dep_ratio'] = df_climate_factors['ERWR']/df_climate_factors['TRWR']
+        
         df_climate_factors.reset_index(inplace=True)
         
-        return df_rain, df_temp, df_water, df_climate_factors
+        return df_climate_factors
         
     def water_stress(self, water_stress_filename):
+        # Imports and cleans the water stress indicator dataset
         
         # Water stress indicators - 2017
         df_waterstress = pd.read_csv(water_stress_filename,nrows=1970,index_col=False).rename(columns={'Area':'Country'})
         df_waterstress.replace(to_replace = 'Grenade', value = 'Grenada', inplace = True)
         df_waterstress['Country'] = coco.convert(names = df_waterstress['Country'], to='ISO3')
         df_waterstress = df_waterstress[df_waterstress['Year']==2017]
-        df_waterstress = df_waterstress.pivot(index='Country', columns='Variable Name', values='Value').rename(columns={'MDG 7.5. Freshwater withdrawal as % of total renewable water resources':'Water stress (MDG)','SDG 6.4.2. Water Stress':'Water stress (SDG)','SDG 6.4.1. Water Use Efficiency':'Water use efficiency (SDG)' })    
-        df_waterstress.reset_index(inplace=True)
+        df_waterstress = df_waterstress.pivot(index='Country', columns='Variable Name', values='Value')
+        
+        # Rename variables
+        df_waterstress.rename(columns={'MDG 7.5. Freshwater withdrawal as % of total renewable water resources':'WS_MDG','SDG 6.4.2. Water Stress':'WS_SDG','SDG 6.4.1. Water Use Efficiency':'WUE_SDG' },inplace=True)    
              
         return df_waterstress
 
