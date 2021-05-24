4d2694514a2eff61b3cf6f63fe5038763e295299
Herbert Makosa <makosablazej@gmail.com>
2021-05-24 17:12:25 +0100

    fix tools PredModels.make_models

--- dash/tools.py
+++ dash/tools.py
@@ -66,9 +66,9 @@ class PredModels():
             'regressor__alpha': alphas_test
             }]
         
-        gridsearch_ws_mdg = GridSearchCV(pipe_climate, test_params, verbose=1, n_jobs=-1).fit(np.asarray(self.df_pred), np.asarray(self.df_target['WS_MDG']))
-        gridsearch_wue_sdg = GridSearchCV(pipe_climate, test_params, verbose=1, n_jobs=-1).fit(np.asarray(self.df_pred), np.asarray(self.df_target['WUE_SDG']))
-        gridsearch_ws_sdg = GridSearchCV(pipe_climate, test_params, verbose=1, n_jobs=-1).fit(np.asarray(self.df_pred), np.asarray(self.df_target['WS_SDG']))
+        gridsearch_ws_mdg = GridSearchCV(model_pipe, test_params, verbose=1, n_jobs=-1).fit(np.asarray(self.df_pred), np.asarray(self.df_target['WS_MDG']))
+        gridsearch_wue_sdg = GridSearchCV(model_pipee, test_params, verbose=1, n_jobs=-1).fit(np.asarray(self.df_pred), np.asarray(self.df_target['WUE_SDG']))
+        gridsearch_ws_sdg = GridSearchCV(model_pipe, test_params, verbose=1, n_jobs=-1).fit(np.asarray(self.df_pred), np.asarray(self.df_target['WS_SDG']))
         
         model_ws_mdg = gridsearch_ws_mdg.best_estimator_
         model_ws_mdg.fit(np.asarray(self.df_pred), np.asarray(self.df_target['WS_MDG']))
